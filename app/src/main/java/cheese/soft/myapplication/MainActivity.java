825bba264d6ca0d3ec1ac20a7323e2748adc881c
Cheese <svvcheese@gmail.com>
2018-10-02 16:42:43 +0300

    Revert "xz"
    This reverts commit 6d618c6

--- app/src/main/java/cheese/soft/myapplication/MainActivity.java
+++ app/src/main/java/cheese/soft/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package cheese.soft.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
     }
 }
